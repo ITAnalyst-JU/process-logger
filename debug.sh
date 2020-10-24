b74eea22fcbc28e27a646731cec70da62dbce913
Juliusz Wajgelt <jwajgelt@gmail.com>
2020-10-24 11:53:51 +0200

    use a named pipe for strace output

--- debug.sh
+++ debug.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-strace -f -tt  $1  2>&1| python3 logger.py
+FIFO_NAME='/tmp/logger.'$$
+
+mkfifo $FIFO_NAME
+strace -f -tt -o $FIFO_NAME $1 &
+python3 logger.py < $FIFO_NAME
+rm $FIFO_NAME
