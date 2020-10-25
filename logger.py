a1845bf64aef78753c4ded3ad94fe4415a8ead32
JHodor <jedrzej.hodor@gmail.com>
2020-10-25 14:08:51 +0100

    Add basic html and db

--- logger.py
+++ logger.py
@@ -1,27 +1,87 @@
 import fileinput
 import re
+from pyhtml import *
+from tinydb import TinyDB, Query
 
-#regexy i nazwy funkcji do wywołania na liniach typu write, duplikacje portów, exec, exit...
 line_types = [
     {"name": "write", "regex": "write", "function": "write_func"}
 ]
 
+class Parsed_line:
+    def __init__(self, time, desc, text):
+        self.time = time
+        self.desc = desc
+        self.text = text
+
+    def my_to_string(self):
+        return "TIME: " + self.time + " DESCRIPTOR: " + self.desc + " CONTENT: " + self.text
+
+    def my_to_dict(self):
+        return {
+            "time": self.time,
+            "desc": self.desc,
+            "text": self.text
+        }
 
 def write_func(line):
     line = re.sub("\s", "", line)
     #print(line)
-    time = line[0:15]
+    time = line[5:13]
     desc = re.findall("write\((.*),.*,",line)
     desc = desc[0]
     text = re.findall("write\(.*,\"(.*)\",",line)
     text = text[0]
-    print(time, desc, text)
+    return Parsed_line(time, desc, text)
 
 
-#glowna petla czyta stdin i dla kazdej lini jak pasuje do wzorca odpala odpowiednia funkcje
-#glowna petla czyta stdin i dla kazdej lini jak pasuje do wzorca odpala odpowiednia funkcje
+#https://pypi.org/project/PyHTML/
+def f_links(ctx):
+    for title, page in [('Home', './index.html')]:
+        yield li(a(href=page)(title))
+
+def create_home_page(content_list):
+    items = lambda ctx: list(map(lambda x: li(div(x)), content_list))
+    return html(
+        head(
+            title('Logger'),
+            #script(src="http://path.to/script.js")
+        ),
+        body(
+            header(
+                img(src='./resources/logo.png'),
+                nav(
+                    ul(f_links)
+                )
+            ),
+            ul(items),
+            footer(
+                hr,
+                'Jagiellonian University: Logger Team'
+            )
+        )
+    ).render()
+
+def lines_to_dict(content_list):
+    data = {"name": "some_execution_name"}
+    parsed_lines = {}
+    for i in range(len(content_list)):
+        parsed_lines[str(i)] = content_list[i].my_to_dict()
+    data["lines"] = parsed_lines
+    return data
+
+#MAIN:
+lines = []
 for line in fileinput.input():
     for line_type in line_types:
-        if len(re.findall(line_type["regex"], line))>0:
-            eval(line_type["function"])(line) #odpala funkcje po nazwie zeby nie robic wielkiego ifa, wszystkie funkcje powiinny przyjmowac jeden parametr-linie
+        if len(re.findall(line_type["regex"], line)) > 0:
+            lines.append(eval(line_type["function"])(line))
 
+html_file = open("index.html", "w+")
+html_content = create_home_page(list(map(lambda x: x.my_to_string(), lines)))
+#print(html_content)
+html_file.write(html_content)
+
+db_file = open("./local_data/db.json", "w+")
+db_file.close()
+db = TinyDB('./local_data/db.json')
+db.insert(lines_to_dict(lines))
--- readme.md
+++ readme.md
@@ -1,3 +1,7 @@
 ## problemy
 strace pisze na stderr więc trzeba odfiltrowywać od stderr programu. To jest coś co może psuć raz na milion przypadków
 
+## Środowisko:
+`pip install pyhtml` <- dokumentacja jest w pliku, który się zainstaluje
+
+`pip install tinydb` 
